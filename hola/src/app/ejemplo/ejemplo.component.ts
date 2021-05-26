098629701a4a80f87eb87353fc8a50c00db70eaf
deel98 <77516339+deel98@users.noreply.github.com>
2021-05-25 20:22:44 -0500

    ngFor
    opciones con subrayado ngClass

--- hola/src/app/ejemplo/ejemplo.component.ts
+++ hola/src/app/ejemplo/ejemplo.component.ts
@@ -3,10 +3,12 @@ import { Component, OnInit } from '@angular/core';
 class Alumno {
   nombre!: string;
   apellido!: string;
+  excelencia!: boolean;
 
   constructor() {
     this.nombre = this.nombre;
     this.apellido = this.apellido;
+    this.excelencia = this.excelencia;
   }
 }
 
@@ -27,10 +29,11 @@ export class EjemploComponent implements OnInit {
     this.listadoAlumnos.push({
       nombre: this.alumno.nombre,
       apellido: this.alumno.apellido,
+      excelencia: this.alumno.excelencia,
     });
     this.alumno.nombre = '';
     this.alumno.apellido = '';
-
+    this.alumno.excelencia = false;
     console.log(this.listadoAlumnos);
   }
 }
