0df241b55639f9b7baf5a247b945c6e04edd4db4
deel98 <77516339+deel98@users.noreply.github.com>
2021-05-25 19:51:13 -0500

    ngFor example
    ejemplo de una lista de productos

--- hola/src/app/ejemplo/ejemplo.component.ts
+++ hola/src/app/ejemplo/ejemplo.component.ts
@@ -1,12 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 
+class Alumno {
+  nombre!: string;
+  apellido!: string;
+
+  constructor() {
+    this.nombre = this.nombre;
+    this.apellido = this.apellido;
+  }
+}
+
 @Component({
   selector: 'app-ejemplo',
   templateUrl: './ejemplo.component.html',
   styleUrls: ['./ejemplo.component.scss'],
 })
 export class EjemploComponent implements OnInit {
+  alumno: Alumno = new Alumno();
+  listadoAlumnos: Array<Alumno> = new Array<Alumno>();
+
   constructor() {}
 
   ngOnInit(): void {}
+
+  agregar() {
+    this.listadoAlumnos.push({
+      nombre: this.alumno.nombre,
+      apellido: this.alumno.apellido,
+    });
+    this.alumno.nombre = '';
+    this.alumno.apellido = '';
+
+    console.log(this.listadoAlumnos);
+  }
 }
